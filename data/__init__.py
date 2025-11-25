715cb3d0be5ef2cd99c96714a6bc4ee4d55516f0
hsuuxyuu997 <hxy2197@gmail.com>
2025-11-25 22:44:17 +0800

    add dataset documentation modify data/__init__,py

--- data/__init__.py
+++ data/__init__.py
@@ -1,23 +1,6 @@
-from torch.utils.data import DataLoader
-from dataset import Synth90kDataset, synth90k_collate_fn
+from .dataset import Synth90kDataset, synth90k_collate_fn
 
-root = r"mnt/ramdisk/max/90kDICT32px"
-
-dataset = Synth90kDataset(
-    root_dir= root,
-    mode="train",
-)
-
-batch = 8
-loader = DataLoader(
-    dataset,
-    batch_size=batch,
-    collate_fn = synth90k_collate_fn
-)
-
-for images, target, length in loader:
-    print("img size:", images.shape)
-    print("target:", target)
-    print("length:", length)
-
-    break
+__all__=[
+    "Synth90kDataset",
+    "synth90k_collate_fn",
+]
